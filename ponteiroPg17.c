4e4c9185478bc72ac859419ad0667093f0c2dedc
Henrique Assis <154931368+Jose-Henrique-a@users.noreply.github.com>
2024-06-03 23:33:28 -0300

    feat

--- ponteiroPg17.c
+++ ponteiroPg17.c
@@ -1,1 +1,20 @@
-// comentário
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    int array[5] = {1,2,3,4,5};
+    int *pointer;
+    pointer = &array[0];
+    
+    printf("\nPrimeiro Elemento: %d", *pointer);
+    pointer++;
+    printf("\nSegundo Elemento: %d", *pointer);
+    pointer++;
+    printf("\nTerceiro Elemento: %d", *pointer);
+    int i;
+    
+    for(i=0; i<5; i++)
+        printf("\nArray[%d]=%d", i, array[i]);
+    return 0;
+}
